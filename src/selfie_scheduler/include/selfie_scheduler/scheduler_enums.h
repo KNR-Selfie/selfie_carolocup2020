1b2f114aeeb81e2ac28bcf7184fb4f613532a1b6
magrim <marczuk@live.it>
2019-05-27 20:45:50 +0200

    added basic state enums

--- src/selfie_scheduler/include/selfie_scheduler/scheduler_enums.h
+++ src/selfie_scheduler/include/selfie_scheduler/scheduler_enums.h
@@ -2,10 +2,29 @@
 #define SCHEDULER_ENUMS_H
 typedef enum program_states
 {
-    SELFIE_READY = 0,// – auto gotowe do jazdy, oczekuje na otrzymanie sygnału startu,
-    START_SIGN,// – auto otrzymało sygnał startu,
-    START_DRIVE,// – auto rozpoczęło jazdę,
-    END_DRIVE, // – auto przejechało wymagany dystans
-}feedback_var;
+    //starting_procedure
+    SELFIE_READY = 0, // car ready, waiting for button press
+    START_SIGN, // button pressed
+    START_DRIVE, // car started to drive
+    END_DRIVE, // car drove given distance
+
+    //track ride
+    AUTONOMOUS_DRIVE, // autonomous ride
+    DETECT_START_LINE, // start line detected
+    DETECT_CROSS_LINE, // cross line detected
+    DETECT_CROSSROAD, // intersection detected
+    DETECT_OBSTACLE, // obstacle detected
+    CHANGE_LANE, // change lane
+
+    //parking
+    START_SEARCHING_PLACE, //start to search parking place
+    FIND_PLACE, // parking place found (don't know if suitable)
+    FIND_PROPER_PLACE, // suitable parking place found
+    START_PARK, // parking procedure started
+    IN_PLACE, // car parked
+    OUT_PLACE, // car drives out of parking place
+    READY_TO_DRIVE, // car ready to further ride
+
+}feedback_variable;
 
 #endif // SCHEDULER_ENUMS_H
