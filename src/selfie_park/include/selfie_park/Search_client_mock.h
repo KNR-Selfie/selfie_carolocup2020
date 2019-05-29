7c5183e375794a20d57e91c6cfee17915e2737de
pktiuk <kotiuk@wp.pl>
2019-05-30 00:05:33 +0200

    Formatted file

--- src/selfie_park/include/selfie_park/Search_client_mock.h
+++ src/selfie_park/include/selfie_park/Search_client_mock.h
@@ -1,29 +1,25 @@
 #pragma once
 
-#include<stdlib.h>
-#include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include <selfie_msgs/searchAction.h>
 #include <geometry_msgs/Polygon.h>
+#include <ros/ros.h>
+#include <selfie_msgs/searchAction.h>
+#include <stdlib.h>
 
-class Search_client_mock
-{
-    ros::NodeHandle nh_;
-    actionlib::SimpleActionClient<selfie_msgs::searchAction> ac_;
-    public:
-    void send_goal(const float);
-    Search_client_mock(const ros::NodeHandle &nh);
+class Search_client_mock {
+  ros::NodeHandle nh_;
+  actionlib::SimpleActionClient<selfie_msgs::searchAction> ac_;
 
+public:
+  void send_goal(const float);
+  Search_client_mock(const ros::NodeHandle &nh);
 };
 
-Search_client_mock::Search_client_mock(const ros::NodeHandle &nh):
-nh_(nh),
-ac_("search", true)
-{}
+Search_client_mock::Search_client_mock(const ros::NodeHandle &nh)
+    : nh_(nh), ac_("search", true) {}
 
-void Search_client_mock::send_goal(const float spot_length=0.5){
-    selfie_msgs::searchGoal msg;
-    msg.min_spot_lenght=spot_length;//Czy nie za mało/dużo
-    ac_.sendGoal(msg);
-
+void Search_client_mock::send_goal(const float spot_length = 0.5) {
+  selfie_msgs::searchGoal msg;
+  msg.min_spot_lenght = spot_length; // Czy nie za mało/dużo
+  ac_.sendGoal(msg);
 }
