a500d88b576bb49edba773b8848cacdbcad67e91
magrim <marczuk@live.it>
2019-06-08 21:43:46 +0200

    added search action client

--- src/selfie_scheduler/src/scheduler_node.cpp
+++ src/selfie_scheduler/src/scheduler_node.cpp
@@ -1,8 +1,8 @@
 #include <ros/ros.h>
-#include <selfie_scheduler/starting_action_client.h>
+#include <selfie_scheduler/search_action_client.h>
 #include <selfie_scheduler/drive_action_client.h>
-#include <selfie_scheduler/scheduler_enums.h>
-
+#include <selfie_scheduler/starting_action_client.h>
+#include <std_srvs/Empty.h>
 
 
 
@@ -19,8 +19,13 @@ int main(int argc, char **argv)
 
     StartingProcedureClient startingAction("starting_procedure");
     DriveClient driveAction("free_drive");
+    SearchClient searchAction("search");
 
     current_action = STARTING; //dummy - set if all systems launched succesfuly
+
+    ros::ServiceClient resetVision = nh.serviceClient<std_srvs::Empty>("resetVision");
+    ros::ServiceClient cmdStartPub = nh.serviceClient<std_srvs::Empty>("cmd_start_pub");
+    ros::ServiceClient cmdStopPub = nh.serviceClient<std_srvs::Empty>("cmd_stop_pub");
 
     while(ros::ok())
     {
@@ -32,22 +37,40 @@ int main(int argc, char **argv)
             switch(current_action)
             {
                 case STARTING:
+                {
                     previous_action = STARTING;
                     startingAction.waitForServer(200);
                     startingAction.setGoal(float(1.0));
                     break;
+                }
                 case DRIVING:
+                {
                     previous_action = DRIVING;
                     driveAction.waitForServer(200);
                     driveAction.setGoal(startingAction.getResult());
+
+                    std_srvs::Empty reset_msg;
+                    resetVision.call(reset_msg);
+
+                    std_srvs::Empty start_pub;
+                    cmdStartPub.call(start_pub);
                     break;
+                }
                 case PARKING_SEARCH:
+                {
+                    previous_action = PARKING_SEARCH;
+                    searchAction.waitForServer(200);
+                    searchAction.setGoal(float(70.0));
                     break;
+                }
                 case PARK:
+                {
+                    std_srvs::Empty stop_pub;
+                    cmdStopPub.call(stop_pub);
                     break;
+                }
             }
         }
-
         //compare states
         if(current_car_state == previous_car_state)
         {
