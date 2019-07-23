13971bbe283e64c7da4d9edafc57eeb80aa8d601
pktiuk <kotiuk@wp.pl>
2019-07-23 14:45:37 +0200

    Add visualization for intersection action server

--- src/selfie_intersection/include/selfie_intersection/IntersectionServer.h
+++ src/selfie_intersection/include/selfie_intersection/IntersectionServer.h
@@ -21,11 +21,13 @@ private:
   ros::Subscriber obstacles_sub_;
   ros::Publisher visualize_free_place_;
   ros::Publisher speed_publisher_;
+  ros::Publisher visualize_intersection;
 
   float point_min_x_; // Area of interest
   float point_max_x_;
   float point_min_y_;
   float point_max_y_;
+  bool visualization;
 
   std::list<Box> filtered_boxes_;
   selfie_msgs::intersectionFeedback action_status_;
@@ -36,4 +38,5 @@ private:
   void filter_boxes(const selfie_msgs::PolygonArray &);
   void publishFeedback(program_states newStatus);
   void send_goal();
+  void visualizeBoxes(std::list<Box> boxes,float r,float g, float b);
 };
--- src/selfie_intersection/src/IntersectionServer.cpp
+++ src/selfie_intersection/src/IntersectionServer.cpp
@@ -11,12 +11,17 @@ IntersectionServer::IntersectionServer(const ros::NodeHandle &nh, const ros::Nod
   pnh_.param<float>("point_max_x", point_max_x_, 0.95);
   pnh_.param<float>("point_min_y", point_min_y_, -3);
   pnh_.param<float>("point_max_y", point_max_y_, 3);
+  pnh_.param<bool>("visualization", visualization, false);
 }
 
 void IntersectionServer::init()
 {
   obstacles_sub_ = nh_.subscribe("/obstacles", 1, &IntersectionServer::manager, this);
   speed_publisher_ = nh_.advertise<std_msgs::Float64>("/max_speed", 0);
+  if (visualization)
+  {
+    visualize_intersection = nh_.advertise<visualization_msgs::Marker>("/intersection", 10);
+  }
   publishFeedback(STOPPED_ON_INTERSECTION);
   ROS_INFO("Initialized");
 }
@@ -25,13 +30,15 @@ void IntersectionServer::manager(const selfie_msgs::PolygonArray &boxes)
 {
   if (!intersectionServer_.isActive())
   {
-    ROS_INFO("Search server not active");
+    ROS_INFO_THROTTLE(1.5, "Search server not active");
     return;
   }
   filter_boxes(boxes);
   if (filtered_boxes_.size() != 0)
   {
     ROS_INFO_THROTTLE(1.5, "Another car on the road");
+    if (visualization)
+      visualizeBoxes(filtered_boxes_, 1, 0.05, 0.05);
     if (action_status_.action_status != FOUND_OBSTACLES)
       publishFeedback(FOUND_OBSTACLES);
   } else
@@ -79,4 +86,12 @@ void IntersectionServer::publishFeedback(program_states newStatus)
 {
   action_status_.action_status = newStatus;
   intersectionServer_.publishFeedback(action_status_);
+}
+
+void IntersectionServer::visualizeBoxes(std::list<Box> boxes, float r = 1, float g = 1, float b = 1)
+{
+  for (std::list<Box>::iterator iter = boxes.begin(); iter != boxes.end(); iter++)
+  {
+    iter->visualize(visualize_intersection, r, g, b);
+  }
 }
