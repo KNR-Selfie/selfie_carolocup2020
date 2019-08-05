1e6d1f0c5feba97e9d8979b0e6c4eee9d60d204f
pktiuk <kotiuk@wp.pl>
2019-08-05 13:47:34 +0200

    Fotmatted

--- src/selfie_intersection/src/intersection_server.cpp
+++ src/selfie_intersection/src/intersection_server.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "intersection_server");
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
-  IntersectionServer server(nh,pnh);
+  IntersectionServer server(nh, pnh);
 
   ros::spin();
   return 0;
