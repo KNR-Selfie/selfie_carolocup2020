1aaeceabc2b3eca74cadbafe160c71903d7963ab
pktiuk <kotiuk@wp.pl>
2019-09-24 16:45:36 +0200

    Create class Road_obstacle_detector

---
+++ src/selfie_avoiding_obstacles/include/selfie_avoiding_obstacles/road_obstacle_detector.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <ros/ros.h>
+#include <vector>
+
+#include <geometry_msgs/Polygon.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <selfie_msgs/PolygonArray.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Int16.h>
+#include <visualization_msgs/Marker.h>
+
+#include <ros/console.h>
+
+#include <selfie_park/shapes.h>
+
+using namespace std;
+
+class Road_obstacle_detector
+{
+public:
+  Road_obstacle_detector(const ros::NodeHandle &nh, const ros::NodeHandle &pnh);
+  ~Road_obstacle_detector();
+
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber obstacles_sub_;
+  ros::Publisher visualizer_;
+
+
+  std::vector<Box> filtered_boxes_; // boxes are sorted by x valule
+                                            // ascendend (near->far)
+
+  bool visualization_;
+  
+/*
+  bool init();
+  void obstacle_callback(const selfie_msgs::PolygonArray &);  //filters boxes and saves in filtered_boxes_
+  void road_markings_callback(const selfie_msgs::RoadMarkings &); //checks if boxes from filtered_boxes_ are on right lane
+
+
+  void display_places(std::vector<Box> &, const std::string &);
+  void display_place(Box &, const std::string &);
+  */
+};
