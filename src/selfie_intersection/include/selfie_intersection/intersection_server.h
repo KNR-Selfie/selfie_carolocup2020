2968d85709098599c5f5fcf2df7e4b92b49ea561
pktiuk <kotiuk@wp.pl>
2019-09-30 21:57:22 +0200

    Area of interest now depends on distance to intersection.

--- src/selfie_intersection/include/selfie_intersection/intersection_server.h
+++ src/selfie_intersection/include/selfie_intersection/intersection_server.h
@@ -5,6 +5,7 @@
 
 #include <selfie_msgs/PolygonArray.h>
 #include <selfie_msgs/intersectionAction.h>
+#include <std_msgs/Float32.h>
 
 #include <selfie_park/shapes.h>
 #include <selfie_scheduler/scheduler_enums.h>
@@ -20,6 +21,7 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   ros::Subscriber obstacles_sub_;
+  ros::Subscriber intersection_subscriber_;
   ros::Publisher speed_publisher_;
   ros::Publisher visualize_intersection_;
 
@@ -27,6 +29,7 @@ private:
   float point_max_x_;
   float point_min_y_;
   float point_max_y_;
+  float road_width_;
   bool visualization_;
 
   std::list<Box> filtered_boxes_;
@@ -37,6 +40,7 @@ private:
   actionlib::SimpleActionServer<selfie_msgs::intersectionAction> intersectionServer_;
   void init();
   void manager(const selfie_msgs::PolygonArray &);
+  void intersection_callback(const std_msgs::Float32 &);
   void filter_boxes(const selfie_msgs::PolygonArray &);
   void publishFeedback(program_states newStatus);
   void send_goal();
