f7c5ed23967a28602c86a3f57f23b1fede54bbd3
pktiuk <kotiuk@wp.pl>
2019-10-19 15:55:12 +0200

    Mock client sends mock obstacles and mock markings

--- src/selfie_intersection/src/intersection_mock_client.py
+++ src/selfie_intersection/src/intersection_mock_client.py
@@ -3,16 +3,36 @@
 from __future__ import print_function
 import rospy
 import actionlib
+import time
 
+from std_msgs.msg import Float32
+from selfie_msgs.msg import PolygonArray
 import selfie_msgs.msg 
 
 def intersection_client():
     client = actionlib.SimpleActionClient('intersection', selfie_msgs.msg.intersectionAction)
     client.wait_for_server()
-    goal = selfie_msgs.msg.intersectionGoal
+    goal = selfie_msgs.msg.intersectionGoal()
     print("Sending goal")
     client.send_goal(goal)
-    print("Goal sent.Waiting for result")
+    distance_pub=rospy.Publisher('/intersection_distance', Float32, queue_size=10)
+    distance=Float32(data=0.05)
+    time.sleep(0.5)
+    print("Sending mock distance to intersection.")
+    distance_pub.publish(distance)
+    polygons = PolygonArray()
+    pub = rospy.Publisher('/obstacles', PolygonArray, queue_size=10)
+    time.sleep(0.5)
+    pub.publish(polygons)
+    time.sleep(0.5)
+    pub.publish(polygons)
+    time.sleep(0.5)
+    pub.publish(polygons)
+    time.sleep(1)
+    pub.publish(polygons)
+    time.sleep(1)
+    pub.publish(polygons)
+    print('mock obstacles sent')
     client.wait_for_result()
     print("Result achieved")
     return client.get_result()
