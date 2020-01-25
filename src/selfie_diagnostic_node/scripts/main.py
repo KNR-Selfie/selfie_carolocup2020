64f2ca3c108173566e6866ec5913b99ab9e54712
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2020-01-25 14:07:31 +0100

    Logging patter applied

--- src/selfie_diagnostic_node/scripts/main.py
+++ src/selfie_diagnostic_node/scripts/main.py
@@ -39,9 +39,9 @@ def getRosMsgType(type):
   elif type == "Bool":
     return Bool
   else:
-    rospy.logfatal("Incorrect message type, please revise launch file")
+    rospy.logfatal("[Diagnostic Node] Incorrect message type, please revise launch file")
     # TODO Kill process
-    rospy.signal_shutdown("Incorrect message type, please revise launch file")
+    rospy.signal_shutdown("[Diagnostic Node] Incorrect message type, please revise launch file")
     sys.exit(1)
 
 # method blinking selfie lights for defined time
@@ -55,7 +55,7 @@ def blinkLights(time):
 def shutdownPerformance():
   # self.left_blink_.publish(Bool(False))
   # self.right_blink_.publish(Bool(False))
-  print("SHUTDOWN")
+  print("[Diagnostic Node] DIAGNOSER SHUTDOWN")
 
 def getParameters():
   parameters = [] 
@@ -69,10 +69,10 @@ def getParameters():
       device["frequency"] = rospy.get_param("~" + str(i) + "_sensor_hz")
       # print("{} {} {} {} {}".format(device["name"],device["directory"], \
       #                               device["topic"], device["type"], device["frequency"]))
-      rospy.loginfo("%s detected", device["name"])
+      rospy.loginfo("[Diagnostic Node] %s detected", device["name"])
       parameters.append(device)
   except:
-    rospy.loginfo("SUMMARY: %d devices detected",len(parameters))
+    rospy.loginfo("[Diagnostic Node] SUMMARY: %d devices detected",len(parameters))
   return parameters
 
 
@@ -84,7 +84,7 @@ if __name__ =="__main__":
   pub = rospy.Publisher('~selfie_diagnostics', String, queue_size=10) # warning, errors publishers
   left_blink_ = rospy.Publisher("/left_turn_indicator", Bool, queue_size=10)
   right_blink_ = rospy.Publisher("/right_turn_indicator", Bool, queue_size=10)
-  rospy.loginfo("Starting  Selfie Diagnostics")
+  rospy.loginfo("[Diagnostic Node] Starting  Selfie Diagnostics")
   # getting delay when to stop programme
   delay = rospy.get_param("~delay",10)
   # debug value true -> programme spins to infinity
@@ -99,32 +99,26 @@ if __name__ =="__main__":
   rate = rospy.Rate(1)
   # reading start time of node, needed for
   start_time = rospy.get_time() 
-  print("Start")
   while not rospy.is_shutdown():
     checkDevices(devices)
     # checking if diagnostics ended
     if rospy.get_time() - start_time > delay and not debug:
-      print("OVERALL STATUS")
       # showing that diagnostics time is up
       blinkLights(1.0)
       rospy.sleep(0.5)
       # checking overall status
       devices_status = checkDevicesStates(devices)
-      print(devices_status)
-      now = rospy.get_time()
-      diff = rospy.get_time() - start_time
-      print("now: {}, start: {}, diff: {}".format(now, start_time, diff))
       # defining diagnose
       if devices_status == State.FATAL or devices_status == State.ERROR:
-        rospy.logfatal("DIAGNOSE: FATAL ERROR")
+        rospy.logfatal("[Diagnostic Node] FATAL ERROR")
       elif devices_status == State.WARNING:
-        rospy.loginfo("DIAGNOSE: WARNING")
+        rospy.loginfo("[Diagnostic Node] WARNING")
         blinkLights(6.0)
       elif devices_status == State.OK:
-        rospy.loginfo("DIAGNOSE: OK")
+        rospy.loginfo("[Diagnostic Node] OK")
         blinkLights(2.0)
       else:
-        rospy.loginfo("DIAGNOSE: WTF")
-      rospy.signal_shutdown("Diagnostics Done")
+        rospy.loginfo("[Diagnostic Node] WTF")
+      rospy.signal_shutdown("[Diagnostic Node] Done")
       sys.exit(0)         
     rate.sleep()
