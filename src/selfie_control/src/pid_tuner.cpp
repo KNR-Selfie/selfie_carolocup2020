2656e44ceeab933e4fe5ca88e10681ff940d63fd
Dobry123 <mat.szczygielski1@gmail.com>
2020-02-01 15:30:43 +0100

    wip

---
+++ src/selfie_control/src/pid_tuner.cpp
@@ -0,0 +1,97 @@
+#include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <std_srvs/Empty.h>
+#include <selfie_control/PIDTunerConfig.h>
+#include <dynamic_reconfigure/server.h>
+
+#include <pid/PidConfig.h>
+#include <dynamic_reconfigure/Config.h>
+
+float act_speed = 1.0;
+bool running = true;
+
+float kp_base = 1.0;
+float speed_base = 1.0;
+float coeff = 0.5;
+float deadzone = 0.1;
+float kp_base_scale = 1;
+
+void speedCallback(const std_msgs::Float32 &msg)
+{
+  act_speed = msg.data;
+}
+
+bool startRunningCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+  running = true;
+  ROS_INFO("PID_tuner started");
+  return true;
+}
+
+bool stopRunningCallback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
+{
+  running = false;
+  ROS_INFO("PID_tuner stopped");
+  return true;
+}
+
+void reconfigureCB(selfie_control::PIDTunerConfig& config, uint32_t level);
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "pid_tuner");
+
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  pnh.getParam("kp_base", kp_base);
+  pnh.getParam("speed_base", speed_base);
+  pnh.getParam("coeff", coeff);
+  pnh.getParam("deadzone", deadzone);
+
+  ROS_INFO("kp_base: %.3f", kp_base);
+  ROS_INFO("speed_base: %.3f", speed_base);
+  ROS_INFO("coeff: %.3f", coeff);
+  ROS_INFO("deadzone: %.3f\n", deadzone);
+  ROS_INFO("pid_tuner initialized");
+
+  ros::Subscriber sub_speed = nh.subscribe("stm32/speed", 50, speedCallback);
+  ros::ServiceServer run_service = nh.advertiseService("/PID_tuner_start", startRunningCallback);
+  ros::ServiceServer not_run_service = nh.advertiseService("/PID_tuner_stop", stopRunningCallback);
+
+  dynamic_reconfigure::Server<selfie_control::PIDTunerConfig> dr_server_;
+  dynamic_reconfigure::Server<selfie_control::PIDTunerConfig>::CallbackType dr_server_CB_;
+  dr_server_CB_ = boost::bind(&reconfigureCB, _1, _2);
+  dr_server_.setCallback(dr_server_CB_);
+
+  // variables used for changing settings of PID
+  dynamic_reconfigure::ReconfigureRequest srv_req_;
+  dynamic_reconfigure::ReconfigureResponse srv_resp_;
+  dynamic_reconfigure::DoubleParameter double_param_;
+  dynamic_reconfigure::Config conf_;
+
+  ros::Rate loop_rate(10);
+
+  while (nh.ok())
+  {
+    // check for incoming messages
+    ros::spinOnce();
+
+    if(running)
+    {
+      float speed_diff = act_speed - speed_base;
+      float kp_diff = speed_diff * coeff;
+      if (std::abs(kp_diff) > deadzone)
+      {
+        float new_kp = kp_base + kp_diff;
+      }
+    }
+
+    loop_rate.sleep();
+  }
+}
+
+void reconfigureCB(selfie_control::PIDTunerConfig& config, uint32_t level)
+{
+  ;
+}
