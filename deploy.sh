5dac51455bd672ca45c8759116ad711b36944c5b
RafLit <37085820+RafLit@users.noreply.github.com>
2020-02-08 17:46:45 +0100

    Deploy (#85)
    * deploy script
    
    * Only deploy
    
    * optional devel build delete
    
    * status msg cleanup
    
    * readme
    
    * readme update
    
    Co-authored-by: Mateusz Szczygielski <mat.szczygielski1@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+selfie_ip="10.10.1.1"
+selfie_name="selfie"
+
+echo "selfie password:"
+read -s password
+
+deleted="/home/selfie/selfie_carolocup2020/src"
+
+while getopts ":d" opt; do
+  case ${opt} in
+    d )
+      deleted="${deleted} /home/selfie/selfie_carolocup2020/build /home/selfie/selfie_carolocup2020/devel"
+      ;;
+    \? ) echo "Usage: ./deploy.sh [-d]"
+      ;;
+  esac
+done
+echo "deleting workspace"
+sshpass -p "$password" ssh "${selfie_name}@${selfie_ip}" rm -r $deleted && echo "deleted remote workspace"
+echo "copying workspace"
+sshpass -p "$password" scp -p -r src "${selfie_name}@${selfie_ip}":/home/selfie/selfie_carolocup2020/src && echo "workspace copied"
+
