c11c19f15b91e49be6beb9fa1e6b3426cc061346
Dobry123 <mat.szczygielski1@gmail.com>
2020-02-08 12:25:50 +0100

    Only deploy

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,11 @@
 #!/usr/bin/env bash
 selfie_ip="10.10.1.1"
 selfie_name="selfie"
-my_ip="192.168.0.116"
 
 echo "selfie password:"
 read -s password
 
-export ROS_MASTER_URI=$selfie_ip
-export ROS_IP=$my_ip
-
-
-sshpass -p "$password" ssh "${selfie_name}@${selfie_ip}" rm -r /home/rav/Code/selfie_carolocup2020/src && echo "deleted remote workspace"
-sshpass -p "$password" scp -r devel "${selfie_name}@${selfie_ip}":/home/rav/Code/selfie_carolocup2020/src && echo "workspace copied"
+sshpass -p "$password" ssh "${selfie_name}@${selfie_ip}" rm -r /home/selfie/selfie_carolocup2020/src /home/selfie/selfie_carolocup2020/build /home/selfie/selfie_carolocup2020/devel && echo "deleted remote workspace"
+sshpass -p "$password" scp -r src "${selfie_name}@${selfie_ip}":/home/selfie/selfie_carolocup2020/src && echo "workspace copied"
 
 echo "copied"
-
-sshpass -p "$password" ssh -t "${selfie_name}@${selfie_ip}" ' 
-screen -S selfie_screen  -dm
-screen -S selfie_screen -X stuff "cd /home/rav/Code/selfie_carolocup2020\n"
-
-screen -S selfie_screen -X stuff "export ROS_MASTER_URI=10.10.1.1\n"
-
-screen -S selfie_screen -X  stuff "export ROS_IP=10.10.1.1\n"
-screen -S selfie_screen -X  stuff "cd /home/rav/Code/selfie_carolocup2020\n"
-
-screen -S selfie_screen -X stuff "catkin_make\n"
-screen -S selfie_screen -X stuff "source devel/setup.bash\n"
-screen -S selfie_screen -X stuff "roslaunch selfie_launch all_debug_rviz.launch\n"
-screen -S selfie_screen -r
-'
-
