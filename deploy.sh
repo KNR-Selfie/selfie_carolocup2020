b42242393a0e0e0e7e45dd88d806919dfd91ac19
Rafal Litka <rafal.litka.stud@pw.edu.pl>
2020-02-08 15:04:57 +0100

    optional devel build delete

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,20 @@ selfie_name="selfie"
 echo "selfie password:"
 read -s password
 
-sshpass -p "$password" ssh "${selfie_name}@${selfie_ip}" rm -r /home/selfie/selfie_carolocup2020/src /home/selfie/selfie_carolocup2020/build /home/selfie/selfie_carolocup2020/devel && echo "deleted remote workspace"
-sshpass -p "$password" scp -r src "${selfie_name}@${selfie_ip}":/home/selfie/selfie_carolocup2020/src && echo "workspace copied"
+deleted="/home/selfie/selfie_carolocup2020/src"
+
+while getopts ":d" opt; do
+  case ${opt} in
+    d )
+      deleted="${deleted} /home/selfie/selfie_carolocup2020/build /home/selfie/selfie_carolocup2020/devel"
+      echo $deleted
+      ;;
+    \? ) echo "Usage: ./deploy.sh [-d]"
+      ;;
+  esac
+done
+echo "start"
+sshpass -p "$password" ssh "${selfie_name}@${selfie_ip}" rm -r $deleted && echo "deleted remote workspace"
+sshpass -p "$password" scp -p -r src "${selfie_name}@${selfie_ip}":/home/selfie/selfie_carolocup2020/src && echo "workspace copied"
 
 echo "copied"
