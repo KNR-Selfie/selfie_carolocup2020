b1f53a5a343a94b7573049f5b48667d0564f42cf
Michał Kiełczykowski <michal.k.kielczykowski@gmail.com>
2020-02-09 21:32:28 +0100

    Lidar detecting added

--- src/selfie_starting_procedure/src/scanner.cpp
+++ src/selfie_starting_procedure/src/scanner.cpp
@@ -6,60 +6,111 @@
 GateScanner::GateScanner(const ros::NodeHandle &nh, const ros::NodeHandle &pnh):
 nh_(nh), pnh_(pnh)
 {
-  pnh_.param<float>("no_obstacle_time_thresh", no_obstacle_time_thresh_, 1.5f);
-  pnh_.param<float>("min_distance", min_distance_, 0.1);
-  pnh_.param<float>("max_distance", max_distance_, 0.5);
-  pnh_.param<float>("min_width", min_width_, 0.2);
-  pnh_.param<int>("min_gate_seen_count", min_gate_seen_count_, 5);
-  timer_ = nh_.createTimer(ros::Duration(no_obstacle_time_thresh_), &GateScanner::timerCallback, this, true, false);
+  pnh_.param<float>("distance_threshold", distance_threshold_, 1.0);
+  pnh_.param<float>("gate_width", gate_width_, 0.2);
+  pnh_.param<int>("number_of_saved_measurements", number_of_saved_measurements_, 10);
+  pnh_.param<int>("no_obstacles_limit", no_obstacles_limit_, 10);
+  
   start_serv_ = nh_.advertiseService("startGateScan", &GateScanner::startSearching, this);
+  stop_serv_ = nh_.advertiseService("stopGateScan", &GateScanner::stopSearching, this);
   gate_open_pub_ = nh_.advertise<std_msgs::Empty>("scan_gate_open", 1);
-  if(min_gate_seen_count_ < 1) min_gate_seen_count_ = 1;
-  gate_seen_count_ = 0;
+
+  // publishing tresholded pointcloud
+  debug_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("gate_scanner_debug",10);
+}
+
+void GateScanner::appendMeasurement(const float &value)
+{
+  // too little measurements
+  if (measurements_.size() > number_of_saved_measurements_)
+  {
+    measurements_.erase(measurements_.begin());
+  }
+  measurements_.push_back(value);
+}
+
+void GateScanner::checkDistances()
+{
+  // if mean of all measurements is greater than threshold
+  if ((std::accumulate(measurements_.begin(), measurements_.end(), 0.0) /
+                      measurements_.size()) > distance_threshold_ &&
+       measurements_.back() > distance_threshold_)
+  {
+    no_obstacles_counter_++;
+  }
+  else 
+  {
+    no_obstacles_counter_ = 0;
+  }
+}
+
+bool GateScanner::checkValidity(){
+  return (no_obstacles_counter_ >= no_obstacles_limit_) ? true : false;
 }
 
 void GateScanner::laserScanCB(const sensor_msgs::LaserScan &msg)
 {
+  std::vector<float> valid_measurement;
+  ROS_INFO("msg size: %d", static_cast<int>(msg.ranges.size()));
+  sensor_msgs::LaserScan debug_msg = msg;
+  debug_msg.ranges.clear();
   float angle = msg.angle_min;
   for (std::vector<float>::const_iterator it = msg.ranges.begin(); it < msg.ranges.end();
    ++it, angle += msg.angle_increment)
   {
+    // out of bounds
     if (*it > msg.range_max || *it < msg.range_min) continue;
+    // converting to cartesian plane
     Point point(angle, *it);
     {
-      if (point.x > min_distance_ && point.x < max_distance_ && std::abs(point.y) < min_width_ / 2.f)
+      // checking if measurement is in gate range
+      if (std::abs(point.y) < (gate_width_ / 2.f))
       {
-        if(++gate_seen_count_ == min_gate_seen_count_)
-        {
-          timer_.start();
-        }
-        else if(gate_seen_count_ > min_gate_seen_count_)
-        {
-          resetTimer();
-        }
-        return;
-        
+        // finding measurement inside gate range
+        valid_measurement.push_back(point.x);
+        // publishing debug pointcloud
+        debug_msg.ranges.push_back(*it);
+        ROS_INFO("Vector data: %f", *it);
       }
     }
   }
-}
-
-void GateScanner::timerCallback(const ros::TimerEvent &e)
-{
-  gate_open_pub_.publish(std_msgs::Empty());
-  ROS_INFO("timer callback");
-  scan_sub_.shutdown();
-}
-
-void GateScanner::resetTimer()
-{
-  timer_.setPeriod(ros::Duration(no_obstacle_time_thresh_), true);  // reset
-  ROS_INFO("timer reset");
+  // if lidar doesnt produce any data in this direction it means 
+  //gate is opened
+  if (valid_measurement.size() == 0) 
+  {
+    no_obstacles_counter_++;
+    ROS_INFO("Gate Scanner: counter state %d", no_obstacles_counter_);
+    return;
+  }
+  float mean = std::accumulate(valid_measurement.begin(), valid_measurement.end(), 0.0) / 
+                               valid_measurement.size();
+  ROS_INFO("Measurement mean: %f", mean);
+  debug_scan_pub_.publish(debug_msg);
+  appendMeasurement(mean);
+  // check if measurements are greater than treshold distance
+  checkDistances();
+  // check if gate is open
+  if (checkValidity()) 
+  {
+    gate_open_pub_.publish(std_msgs::Empty());
+    ROS_INFO("GateScanner: gate open");
+    no_obstacles_counter_ = 0;
+    scan_sub_.shutdown(); 
+  }
+  ROS_INFO("Gate Scanner: counter state %d", no_obstacles_counter_);
 }
 
 bool GateScanner::startSearching(std_srvs::Empty::Request &rq, std_srvs::Empty::Response &rp)
 {
   scan_sub_ = nh_.subscribe("scan", 10, &GateScanner::laserScanCB, this);
+  ROS_INFO("GateScanner: start searching");
+  return true;
+}
+
+bool GateScanner::stopSearching(std_srvs::Empty::Request &rq, std_srvs::Empty::Response &rp)
+{
+  scan_sub_.shutdown();
+  ROS_INFO("GateScanner: stopped searching");
   return true;
 }
 
